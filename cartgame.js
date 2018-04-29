e35d0bb35ca95a1a89488288c23bf57c9fc83fc2
Kevin Lo <niniy111@hotmail.com>
2018-04-28 22:47:12 -0700

    Added working timer on click

--- cartgame.js
+++ cartgame.js
@@ -1,20 +1,44 @@
+var startButton = document.createElement("Button");
+startButton.innerHTML = "Start Game";
+
+var gameStarted = 0;
+
+var x;//get horizontal coord
+var y;//get vertical coord
+
 var difficulty = 1;
-var gameTime = 60;
-var timer = setInterval(timerFunc, 1000);
+var gameTime = 60; //The game time per round until difficulty increases
+var timer; //used as the variable for the time counter
 
 var canvas = document.getElementById("gameSpace");
 
 var timerWords = canvas.getContext("2d");
 
+function showCoords(event) { //justshows the coordinates for now
+    x = event.clientX;
+    y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coords;
+}
 
-function timerFunc() {
+function timerStart() { //used to start the timer when the canvas is clicked
+    if(gameStarted == 0) {
+        gameStarted = 1;
+        timer = setInterval(timerFunc, 1000);
+    }  
+}
+
+function timerFunc() { //is the timer
+    timerWords.clearRect(0,0,150,25);  
     if(gameTime == -1) {
         gameTime = 60;
         difficulty++;
     }
-    timerWords.font = "30px Arial";
-    timerWords.strokeText("Time Left: " + gameTime,5,30);
+    timerWords.font = "20px Arial";
+    timerWords.strokeText("Time Left: " + gameTime,5,20);
     gameTime--;
 }
+
+
 
 
