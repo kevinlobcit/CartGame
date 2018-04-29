4b85bb088e96da9b173bd6898c60966d0bc48ce5
Kevin Lo <klo74@my.bcit.ca>
2018-04-29 15:23:28 -0700

    added button to enter and exit fullscreen

--- cartgame.js
+++ cartgame.js
@@ -1,7 +1,11 @@
 var startButton = document.createElement("Button");
 startButton.innerHTML = "Start Game";
 
-var gameStarted = 0;
+var fullscreen = 0;
+var fulScrnBtn = document.getElementById("fulscrnBtn");
+
+
+var gameStarted = 0; //use to prevent the start of more timers after clicking the game once
 
 var difficulty = 1;
 var gameTime = 60; //The game time per round until difficulty increases
@@ -10,6 +14,39 @@ var timer; //used as the variable for the time counter
 var canvas = document.getElementById("gameSpace");
 
 var timerWords = canvas.getContext("2d");
+
+function goFullscreen() {
+    if(fullscreen == 0) {
+        var docElm = document.documentElement;
+        if(docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        }
+        else if(docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        }
+        else if(docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        }
+        else if(docElm.msRequestFullscreen) {
+            docElm.msRequestFullscreen();
+        }
+        fulScrnBtn.innerHTML = "Exit Fullscreen";
+        fullscreen = 1;
+    }
+    else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+        fulScrnBtn.innerHTML = "Enter Fullscreen";
+        fullscreen = 0;
+    }
+}
 
 function timerStart() { //used to start the timer when the canvas is clicked
     if(gameStarted == 0) {
